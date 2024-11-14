47e4679877e22ac7e5fa4b081cb4d47748f8db73
junaedi.junz38 <junaedi.junz38@gmail.com>
2024-11-14 09:58:26 +0000

    add layout signup

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -4,7 +4,6 @@ import { SignInCard } from "@/features/auth/components/sign-in-card";
 
 const SignInPage = () => {
     return <SignInCard />;
-    // return <div className="p-7">budi</div>;
 };
 
 export default SignInPage;
--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,9 +1,10 @@
-const SignUpPage = () =>{
-    return (
-        <div>
-            signUp
-        </div>
-    );
-}
+"use client";
+
+import { SignUpCard } from "@/features/auth/components/sign-up-card";
+
+const SignUpPage = () => {
+    return <SignUpCard />;
+    return <div className="px-7">as</div>;
+};
 
 export default SignUpPage;
