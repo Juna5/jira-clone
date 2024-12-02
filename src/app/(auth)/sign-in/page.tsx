cd9ff8255c79825cd878c632dbc116142fbd3aa0
Juna5 <junaedi.junz38@gmail.com>
2024-12-02 15:11:08 +0700

    fix auth layout

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -4,6 +4,7 @@ import { SignInCard } from "@/features/auth/components/sign-in-card";
 
 const SignInPage = () => {
     return <SignInCard />;
+    return <div className="text-blue-700"> as</div>;
 };
 
 export default SignInPage;
--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -4,7 +4,7 @@ import { SignUpCard } from "@/features/auth/components/sign-up-card";
 
 const SignUpPage = () => {
     return <SignUpCard />;
-    return <div className="px-7">as</div>;
+    return <div className="gap-y-4">as</div>;
 };
 
 export default SignUpPage;
